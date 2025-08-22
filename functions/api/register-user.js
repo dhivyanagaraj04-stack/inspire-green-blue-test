6841da4b96421168be81256544a8d1288bc97f7c
dxn310301@darden.com <dxn310301@darden.com>
2025-08-22 19:56:48 +0530

    store changes added

--- functions/api/register-user.js
+++ functions/api/register-user.js
@@ -1,7 +1,7 @@
 export async function onRequestPost(context) {
     const formdata = await context.request.formdata();
     const user_name = formdata.get("user");
-    const password = formdata.get("pswd");
+    const password = formdata.get("psw");
     await context.env.USER_DATA_STORE.put(user_name, password);
     return new Response(`${user_name} and ${password}`);
 }
