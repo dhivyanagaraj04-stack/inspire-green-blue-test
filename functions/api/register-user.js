6eb697375c1d7af438eb2985a2fb4be5d6cc6ecf
dxn310301@darden.com <dxn310301@darden.com>
2025-08-22 20:36:19 +0530

    form changes

--- functions/api/register-user.js
+++ functions/api/register-user.js
@@ -1,5 +1,5 @@
 export async function onRequestPost(context) {
-    const formdata = await context.request.formdata();
+    const formdata = await context.request.formData();
     const user_name = formdata.get("user");
     const password = formdata.get("psw");
     await context.env.USER_DATA_STORE.put(user_name, password);
