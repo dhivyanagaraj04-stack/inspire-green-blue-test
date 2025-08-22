04cd730dc4dea8e817c037b40254464e9c78398b
dxn310301@darden.com <dxn310301@darden.com>
2025-08-22 17:41:41 +0530

    kv added

--- functions/api/register-user.js
+++ functions/api/register-user.js
@@ -1,6 +1,7 @@
 export async function onRequestPost(context) {
     const formdata = await context.request.formdata();
     const user_name = formdata.get("user");
-       const password = formdata.get("pswd");
+    const password = formdata.get("pswd");
+    await context.env.USER_DATA_STORE.put(user_name, password);
     return new Response(`${user_name} and ${password}`);
 }
