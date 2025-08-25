32a5e73ff6d2906f8fe684bdbf0e6aacf2b5b5b7
dxn310301@darden.com <dxn310301@darden.com>
2025-08-25 11:56:18 +0530

    fetch data

--- functions/api/register-user.js
+++ functions/api/register-user.js
@@ -2,6 +2,11 @@ export async function onRequestPost(context) {
     const formdata = await context.request.formData();
     const user_name = formdata.get("user");
     const password = formdata.get("psw");
-    await context.env.USER_DATA_STORE.put(user_name, password);
+    // await context.env.USER_DATA_STORE.put(user_name, password);
+    const getData = await context.env.USER_DATA_STORE.get();
+    console.log(getData, 'test');
+    
     return new Response(`${user_name} and ${password}`);
 }
+
+ 
